8c5aac9107b44059b78a5ed92bff55bd83cd16de
Jabeen Cheema <jabeencheema@pursuit.org>
2018-11-08 17:36:44 -0500

    testing

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
+// this is where you should have your logic, win or loose, 
+
 import Foundation
 
 class TicTacToeBrain {
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -6,13 +6,17 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
+// this is on this is off
 import UIKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
+    
+    
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    print("we are making tic tac toe!")
   }
 
 
