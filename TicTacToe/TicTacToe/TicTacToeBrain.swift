046db002364700c725d8849e74ea16dca6cba86a
SLRAM <stephier2290@gmail.com>
2018-11-12 01:57:32 -0500

    missing diagnal wins and refactors

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,62 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class TicTacToeBrain {
-  
+
+    
+    static func assignXO(player: Bool, _ sender: GameButton, text: UILabel) {
+        if player {
+            
+            sender.setImage(UIImage(named: "XTic"), for: .normal)
+            text.text = "Player two's turn!"
+            
+
+            //append col, append row if three of the same number then win or a 0,1,2 in col and row(check for this first)
+            
+            
+        } else {
+            sender.setImage(UIImage(named: "OTac"), for: .normal)
+            text.text = "Player one's turn!"
+        }
+        sender.isEnabled = false
+    }
+
+    static func checkWinnerVertHor(player: Bool,_ sender: GameButton, countColX: [Int], countRowX: [Int], countColO: [Int], countRowO: [Int])-> Bool {
+        if player {
+            print("counter col X is \(countColX)")
+            print("counter Row X is \(countRowX)")
+            for numbers in 0...2 {
+                if countColX.filter({$0 == numbers}).count == 3 {
+                    print("X col win")
+                    return true
+                    
+                }
+                if countRowX.filter({$0 == numbers}).count == 3 {
+                    print("X row win")
+                    return true
+                }
+            }
+        } else {
+            print("counter col O is \(countColX)")
+            print("counter Row O is \(countRowX)")
+            for numbers in 0...2 {
+                if countColO.filter({$0 == numbers}).count == 3 {
+                    print("O col win")
+                    return true
+                    
+                }
+                if countRowO.filter({$0 == numbers}).count == 3 {
+                    print("O row win")
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    static func checkWinnerDiag() {
+        //must use if sender image for 3 buttons are x then win
+    }
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,99 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var topLeft: GameButton!
+    @IBOutlet weak var topMiddle: GameButton!
+    @IBOutlet weak var topRight: GameButton!
+    @IBOutlet weak var middleLeft: GameButton!
+    @IBOutlet weak var middleMiddle: GameButton!
+    @IBOutlet weak var middleRight: GameButton!
+    @IBOutlet weak var botLeft: GameButton!
+    @IBOutlet weak var bottomMiddle: GameButton!
+    @IBOutlet weak var bottomRIght: GameButton!
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    
+
+    var playerOne = true
+    var countColX = [Int]()
+    var countRowX = [Int]()
+    var countColO = [Int]()
+    var countRowO = [Int]()
+    
+    
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
   }
+    func counter(_ sender: GameButton) {
+        if playerOne {
+            countColX.append(sender.col)
+            countRowX.append(sender.row)
+        } else {
+            countColO.append(sender.col)
+            countRowO.append(sender.row)
+        }
+    }
+    func gameOver(winner: Bool) {
+        if winner {
+            topLeft.isEnabled = false
+            topMiddle.isEnabled = false
+            topRight.isEnabled = false
+            middleLeft.isEnabled = false
+            middleMiddle.isEnabled = false
+            middleRight.isEnabled = false
+            botLeft.isEnabled = false
+            bottomMiddle.isEnabled = false
+            bottomRIght.isEnabled = false
+        }
+        if playerOne {
+        turnLabel.text = "Player one wins!"
+        } else {
+            turnLabel.text = "Player two wins!"
+        }
+    }
 
+    @IBAction func allButtons(_ sender: GameButton) {
+//        let row = sender.row
+//        let col = sender.col
+        let sender = sender
+        TicTacToeBrain.assignXO(player: playerOne, sender, text: turnLabel)
+        counter(sender)
+//        TicTacToeBrain.checkWinnerVertHor(player: playerOne, sender, countColX: countColX, countRowX: countRowX, countColO: countColO, countRowO: countRowO)
+        gameOver(winner: TicTacToeBrain.checkWinnerVertHor(player: playerOne, sender, countColX: countColX, countRowX: countRowX, countColO: countColO, countRowO: countRowO))
+        TicTacToeBrain.checkWinnerDiag()
+        playerOne = !playerOne
+        
 
+        
+    }
+
+    @IBAction func newGame(_ sender: UIButton) {
+        topLeft.isEnabled = true
+        topMiddle.isEnabled = true
+        topRight.isEnabled = true
+        middleLeft.isEnabled = true
+        middleMiddle.isEnabled = true
+        middleRight.isEnabled = true
+        botLeft.isEnabled = true
+        bottomMiddle.isEnabled = true
+        bottomRIght.isEnabled = true
+        countColX = [Int]()
+        countRowX = [Int]()
+        countColO = [Int]()
+        countRowO = [Int]()
+        topLeft.setImage(nil, for: .normal)
+        topMiddle.setImage(nil, for: .normal)
+        topRight.setImage(nil, for: .normal)
+        middleLeft.setImage(nil, for: .normal)
+        middleMiddle.setImage(nil, for: .normal)
+        middleRight.setImage(nil, for: .normal)
+        botLeft.setImage(nil, for: .normal)
+        bottomMiddle.setImage(nil, for: .normal)
+        bottomRIght.setImage(nil, for: .normal)
+        playerOne = true
+        
+    }
 }
 
