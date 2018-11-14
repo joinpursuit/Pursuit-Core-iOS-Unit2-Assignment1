5ee481128c5b91590a4300ea6dcc0434eee63cb1
MattHuie <matthuie93@gmail.com>
2018-11-14 03:05:11 -0500

    code done but buttons are connected

--- TicTacToe/TicTacToe/GameViewController.swift
+++ TicTacToe/TicTacToe/GameViewController.swift
@@ -9,22 +9,56 @@
 import UIKit
 
 class GameViewController: UIViewController {
-
+    var ticTacToe = TicTacToeBrain()
+    var gameOver = false
+    var playerOneWins = 0
+    var playerTwoWins = 0
+    
+    @IBOutlet var allTilesButton: [GameButton]!
+    
+    @IBOutlet weak var playerOneWinCountLabel: UILabel!
+    @IBOutlet weak var playerTwoWinCountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var resetScoresButton: UIButton!
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Tic Tac Toe"
+        playerOneWinCountLabel.text = "Player 1 Wins: \(playerOneWins)"
+        playerTwoWinCountLabel.text = "Player 2 Wins: \(playerTwoWins)"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func assigningTiles(_ cardButton: GameButton!) {
+        ticTacToe.checkingConditions(a: cardButton.row, b: cardButton.col)
+        displayLabel.text = ticTacToe.playerTurnCondition
+        if displayLabel.text == "Player One Wins" {
+            playerOneWins += 1
+        }
+        if displayLabel.text == "Player Two Wins" {
+            playerTwoWins += 1
+        }
+        cardButton.setImage(UIImage.init(named: ticTacToe.playerIcon), for: .normal)
+        gameOver = ticTacToe.gameOver
+        if gameOver == true {
+            allTilesButton.forEach{$0.isEnabled = false}
+        }
     }
-    */
-
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+        ticTacToe.resetGame(a: true)
+        allTilesButton.forEach{$0.setImage(UIImage.init(named: "Default Image"), for: .normal)}
+        allTilesButton.forEach{$0.isEnabled = true}
+        displayLabel.text = "Player One's turn!"
+    }
+    
+    @IBAction func resetScoresButton(_ sender: UIButton) {
+        playerOneWins = 0
+        playerTwoWins = 0
+        playerOneWinCountLabel.text = "Player 1 Wins: \(playerOneWins)"
+        playerTwoWinCountLabel.text = "Player 2 Wins: \(playerTwoWins)"
+    }  
 }
+
--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,71 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public struct TicTacToeImage {
+    static let xIcon = "xIcon"
+    static let oIcon = "oIcon"
+}
 
 class TicTacToeBrain {
-  
+    var array = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]]
+    var playerSwitch = true
+    var gameOver = false
+    var playerTurnCondition = ""
+    var playerIcon = ""
+    
+    func checkingConditions (a: Int, b: Int) {
+        var playerValue: Int
+        
+        if playerSwitch {
+            playerValue = 1
+            playerSwitch = false
+            playerTurnCondition = "Player Two's Turn!"
+            playerIcon = TicTacToeImage.xIcon
+            
+        } else {
+            playerValue = 5
+            playerSwitch = true
+            playerTurnCondition = "Player One's Turn!"
+            playerIcon = TicTacToeImage.oIcon
+        }
+        array[a][b] += playerValue
+        var winArray = [[array[0][0],array[1][1],array[2][2]],
+                        [array[2][0],array[1][1],array[0][2]],
+                        [array[0][0],array[0][1],array[0][2]],
+                        [array[1][0],array[1][1],array[1][2]],
+                        [array[2][0],array[2][1],array[2][2]],
+                        [array[0][0],array[1][0],array[2][0]],
+                        [array[0][1],array[1][1],array[2][1]],
+                        [array[0][2],array[1][2],array[2][2]]]
+        for i in 0..<winArray.count {
+            if winArray[i].reduce(0, +) == 3 {
+                playerTurnCondition = "Player One Wins"
+                gameOver = true
+                break
+            } else if winArray[i].reduce(0, +) == 15 {
+                playerTurnCondition = "Player Two Wins"
+                gameOver = true
+                break
+            } else if array.joined().reduce(0, +) == 25 {
+                playerTurnCondition = "Draw"
+                gameOver = true
+            }
+        }
+    }
+    func resetGame(a: Bool){
+        if a == true {
+            array = [
+                [0,0,0],
+                [0,0,0],
+                [0,0,0]]
+            gameOver = false
+            playerSwitch = true
+        }
+    }
+    
 }
