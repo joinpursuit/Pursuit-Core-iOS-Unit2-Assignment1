585687578e3be22d89d6f519e7af85b444e565ab
kwaring3 <kevinwaring@pursuit.org>
2018-11-15 17:01:48 -0500

    Tic Tac Toe1

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -8,15 +8,76 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
-
-    @IBOutlet weak var firstButton: GameButton!
-    override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-        print("hi")
-  }
-
-
+  let player1Turn: Bool = true
+    
+    var gameRunning = true
+    var activePlayer = 1
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    
+    let winningCombinations = [[0,1,2],[0,3,6],[3,4,5],[6,7,8],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+    @IBOutlet var allButtons: [UIButton]!
+    @IBOutlet weak var label: UILabel!
+    @IBAction func Action(_ sender: UIButton)
+    {
+        if (gameState[sender.tag-0] == 0 && gameRunning == true)
+        {
+            gameState[sender.tag-0] = activePlayer
+    
+            if (activePlayer == 1)
+            {
+                sender.setImage(UIImage(named: TicTacToeImageName.X ),for: .normal)
+                activePlayer = 2
+                sender.isEnabled = false
+            }else {
+                sender.setImage(UIImage(named:TicTacToeImageName.O ),for: .normal)
+                activePlayer = 1
+                sender.isEnabled = false
+            }
+        }
+        for combination in winningCombinations {
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]{
+                
+                gameRunning = false
+                if gameState[combination[0]] == 1 {
+                    label.text = "You dont have the answers Sway!!!!"
+                }else{
+                    label.text = " I told her dont wear no 350s round me."
+        }
+                playAgain.isHidden = false
+                label.isHidden = false
+                 allButtons.forEach {$0.isEnabled = false}
 }
+            gameRunning = false
+            
+            for i in gameState{
+                if i == 0 {
+                    gameRunning = true
+                    break
+                }
+            }
+            if gameRunning == false {
+                label.text = " Draw"
+                label.isHidden = false
+                playAgain.isHidden = false
+            }
+           
+}
+        
+}
+    @IBOutlet weak var playAgain: UIButton!
+    @IBAction func playAgain(_ sender: UIButton) {
+        gameState = [0,0,0,0,0,0,0,0,0]
+        gameRunning = true
+        activePlayer = 1
+        playAgain.isHidden = false
+        label.isHidden = false
+        allButtons.forEach {$0.isEnabled = true}
+        allButtons.forEach {$0.setImage(UIImage.init(named: "Default Image"), for: .normal)}
+        label.text = "Player 1 Begin"
+        
 
+    }
+}
