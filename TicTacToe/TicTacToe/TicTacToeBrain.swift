2f3df4b783b4e8f7e1e7b341b3d448ae6eeb7234
John Doe <leandro.wauters@gmail.com>
2018-11-15 14:53:58 -0500

    11/15

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,9 +6,17 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 //this class tells who wins, where things are, who's turn it is
 
 class TicTacToeBrain {
-  
+static var board = [[0,0,0],
+                    [0,0,0],
+                    [0,0,0]]
+static var turn = 1
+static var count = 0
+
 }
+
+
+
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,15 +10,84 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var firstButton: GameButton!
+    @IBOutlet var buttons: [GameButton]!
+    @IBOutlet weak var textWinner: UILabel!
     
+
+    
+
+
     
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("We are making tic tac toe!")
-
+    textWinner.text = "Player One"
+    
   }
+    @IBAction func buttonAction(_ sender: GameButton) {
+        
+            if TicTacToeBrain.turn == 1 {
+                sender.setImage(UIImage.init(named: "x"), for: .normal)
+                TicTacToeBrain.board[sender.row][sender.col] = 1
+                sender.isEnabled = false
+                textWinner.text = "Player Two"
+                TicTacToeBrain.turn = 2
+                TicTacToeBrain.count += 1
+            } else {
+                sender.setImage(UIImage.init(named: "o"), for: .normal)
+                TicTacToeBrain.board[sender.row][sender.col] = 4
+                textWinner.text = "Player One"
+                sender.isEnabled = false
+                TicTacToeBrain.turn = 1
+                TicTacToeBrain.count += 1
+                
+        }
+        let winnerCombos = [[TicTacToeBrain.board[0][0],TicTacToeBrain.board[0][1], TicTacToeBrain.board[0][2]],
+                           [TicTacToeBrain.board[1][0],TicTacToeBrain.board[1][1], TicTacToeBrain.board[1][2]],[TicTacToeBrain.board[2][0],TicTacToeBrain.board[2][1], TicTacToeBrain.board[2][2]],
+                           [TicTacToeBrain.board[0][0],TicTacToeBrain.board[1][0], TicTacToeBrain.board[2][0]],
+                           [TicTacToeBrain.board[0][1],TicTacToeBrain.board[1][1], TicTacToeBrain.board[2][1]],
+                           [TicTacToeBrain.board[0][2],TicTacToeBrain.board[1][2], TicTacToeBrain.board[2][2]],
+                           [TicTacToeBrain.board[0][0],TicTacToeBrain.board[1][1], TicTacToeBrain.board[2][2]],
+                           [TicTacToeBrain.board[2][0],TicTacToeBrain.board[1][1], TicTacToeBrain.board[0][2]]]
+        
+        for index in 0...winnerCombos.count - 1 {
+            let result = winnerCombos[index].reduce(0){$0 + $1}
+            if result == 3 {
+                textWinner.text = "Player one is the winner"
+                for button in buttons {
+                    button.isEnabled = false
+                }
+                break
+            } else if result == 12 {
+                textWinner.text = "Player two is the winner"
+                for button in buttons {
+                    button.isEnabled = false
+                }
+                break
+            } else if TicTacToeBrain.count == 9 {
+                print(result)
+                textWinner.text = "Draw"
+                for button in buttons {
+                    button.isEnabled = false
+                }
+            }
+            
+        }
 
-
+    }
+    @IBAction func restart(_ sender: UIButton) {
+        buttons.forEach{$0.isEnabled = true}
+        buttons.forEach{$0.setImage(UIImage(named: "Default Image"), for: .normal)}
+        TicTacToeBrain.turn = 1
+        TicTacToeBrain.count = 0
+        textWinner.text = ""
+        for index in 0...TicTacToeBrain.board.count - 1 {
+            for num in 0...TicTacToeBrain.board[index].count - 1 {
+                TicTacToeBrain.board[index][num] = 0
+                
+            }
+        }
+        }
+    
+    
 }
 
