70ae03062217ab9f8b3ebcdcaccabba6781c2ab1
Olimpia <olimpiacompean@prusuit.org>
2018-11-18 13:06:43 -0500

    Crashes

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -8,5 +8,110 @@
 
 import UIKit
 
+enum outputOptions {
+    case x
+    case o
+    case tie 
+}
 
+enum checkForWin {
+    case player1Win
+    case player2win
+    case noOneWins
+}
 
+class gameBrain {
+    
+    var gameSquares =  [[outputOptions]](repeating: [outputOptions](repeating: .tie, count: 3), count: 3)
+    
+    var playerTurn = Turns.player1
+
+enum Turns: String, CaseIterable {
+ case player1
+ case player2
+    
+    func image() -> UIImage {
+        var imageButton: UIImage!
+        switch self {
+        case .player1:
+            imageButton = UIImage(named:"XImage")
+        default:
+            imageButton = UIImage(named: "OImage")
+            }
+        return imageButton
+}
+   mutating func switchPlayers() {
+        switch self {
+        case .player1:
+            self = .player2
+        case .player2:
+            self = .player1
+        }
+    }
+ }
+    func gameSetUp(row: Int, col: Int) {
+        let outputs: outputOptions
+        switch playerTurn {
+        case .player1:
+            outputs = .x
+        case .player2:
+            outputs = .o
+        }
+        gameSquares[row][col] = outputs
+        
+        playerTurn.switchPlayers()
+    
+    }
+    func winningConditions() -> checkForWin {
+    
+        for row in gameSquares {
+            if row == [.x, .x, .x] {
+                return .player1Win
+            } else if row == [.o,.o,.o] {
+                return .player2win
+            }
+        }
+        
+        for columnIndex in 0..<gameSquares[0].count {
+            var column = [outputOptions]()
+            for rowIndex in 0..<gameSquares.count {
+                column.append(gameSquares[rowIndex][columnIndex])
+                if column == [.x,.x,.x] {
+                    return .player1Win
+                } else {
+                    if column == [.o,.o,.o] {
+                    return .player2win
+                }
+            }
+        }
+        
+        var across1 = [outputOptions]()
+        for i in 0..<gameSquares.count {
+            across1.append(gameSquares[i][i])
+        }
+        if across1 == [.x,.x,.x] {
+            return .player1Win
+        } else {
+            if across1 == [.o, .o, .o] {
+                return .player2win
+
+            }
+        }
+        
+        var across2 = [outputOptions]()
+        for i in 0..<gameSquares.count {
+             across2.append(gameSquares[i][gameSquares.count - 1 - i])
+        }
+        if across2 == [.x,.x,.x] {
+            return .player1Win
+        } else {
+            if across2 == [.o, .o, .o] {
+                return .player2win
+                
+            }
+        }
+        
+    }
+    return .noOneWins
+}
+}
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var btn2: UIButton!
+    
     @IBOutlet weak var btn3: UIButton!
     @IBOutlet weak var btn4: UIButton!
     @IBOutlet weak var btn5: UIButton!
@@ -19,100 +20,64 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn7: UIButton!
     @IBOutlet weak var btn8: UIButton!
     @IBOutlet weak var btn9: UIButton!
-    var matrixAllTheButtons:[[UIButton]]!
-   var allTheButtons: [UIButton]=[]
-    var arrayOfImages = [UIImage]()
-    var player1 = UIImage.init(named:"OImage")!
-    var player2 = UIImage.init(named:"XImage")!
-    var player1Count = 0
-    var player2Count = 0
+    var gameButton = [UIButton]()
+    
+    var ticTacToeGame: gameBrain = gameBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        matrixAllTheButtons = [
-            [btn1, btn2, btn3],
-            [btn4, btn5, btn6],
-            [btn7, btn8, btn9]]
-        
-        currentPlayer.switchPlayers()
-        
-    }
- var currentPlayer: Players = .player1
-    
-  
+       gameButton = [btn1, btn2, btn2,btn3, btn4, btn5, btn6, btn7, btn8, btn9]
+   
 
-    func resetButton() {
-        for button1 in matrixAllTheButtons {
-            for bt in button1 {
-                bt.isEnabled = true
-              
-            }
-        }
     }
     
-    enum Players {
-        case player1
-        case player2
-        
-        mutating func switchPlayers() {
-            switch self {
-            case .player1:
-                self = .player2
-            case .player2:
-                self = .player1
+      @IBOutlet weak var playAgainLable: UILabel!
+    @IBAction func allButtons(_ sender: GameButton) {
+        let turns = ticTacToeGame.playerTurn
+        sender.setImage(turns.image(), for: .normal)
+        ticTacToeGame.gameSetUp(row: sender.row, col: sender.col)
+        switch ticTacToeGame.winningConditions() {
+        case .player1Win:
+            playAgainLable.text = "Player one: You're the winner!"
+            for button in gameButton {
+                button.isEnabled = false
+            }
+        case .player2win:
+            playAgainLable.text = "Player tow: You're the winner!"
+            for button in gameButton {
+                button.isEnabled = false
+            }
+        case .noOneWins:
+            playAgainLable.text = "No Winners -_-"
+            for button in gameButton {
+                button.isEnabled = false
                 
+                 sender.isEnabled = false
             }
+        }
+        }
+        
+    @IBAction func playButton(_ sender: UIButton) {
+       
+        let turns = ticTacToeGame.playerTurn
+        for button in gameButton {
+            button.isEnabled = true
+            playAgainLable.text = "Click To Play"
         }
         
     }
+    }
     
     
-    func gameOver() {
-        for button in matrixAllTheButtons {
-            for btn in button {
-                btn.isEnabled = false
-            }
-            
-        }
-    }
-    func winningConditionRow1() {
-     if btn1.currentImage == btn2.currentImage && btn2.currentImage == btn3.currentImage {
-        gameOver()
-        }
-    }
 
+    
+    
 
-    var playerTurn = Players.player1
+   
     
-    @IBOutlet weak var player1Label: UILabel!
-    @IBOutlet weak var player2Label: UILabel!
     
-    @IBOutlet weak var playButton: UIButton!
-    @IBAction func allButtons(_ sender: UIButton) {
-    
-        
-        switch currentPlayer {
-        case .player1 :
-            if sender.isEnabled == true {
-                sender.setImage(UIImage.init(named:"OImage"), for: .normal)
-                sender.isEnabled = false
-    }
-               player1Count += 1
-                player1Label.text = "Turn: player 1 (\(player1Count))"
-               
-    
-        case .player2 :
-            if sender.isEnabled == true {
-                sender.setImage(UIImage.init(named:"XImage"), for: .normal)
-                sender.isEnabled = false
-               }
-            player2Count += 1
-            player2Label.text = "Turn: player 2 (\(player2Count))"
-            
-            playerTurn.switchPlayers()
-            playButton.titleLabel!.text = "You win! Click to play again"
-            
-      }
-    }
+         
+
     
     
     
@@ -124,7 +89,7 @@ class ViewController: UIViewController {
  
         
         
-    }
+
    
    
     
