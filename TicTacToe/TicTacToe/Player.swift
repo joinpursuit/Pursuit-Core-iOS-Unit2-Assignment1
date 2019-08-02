c4aed90fafe81605d510ba7977f7461c2f44ec19
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-02 14:58:09 -0400

    creates Player model with player enum and adds skeleton for functions for TicTacTieBrain model

---
+++ TicTacToe/TicTacToe/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Players.swift
+//  TicTacToe
+//
+//  Created by Sunni Tang on 8/2/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import Foundation
+
+enum Player {
+    case playerOne
+    case playerTwo
+}
--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -8,6 +8,41 @@
 
 import Foundation
 
+
 class TicTacToeBrain {
-  
+    var gameBoard: [[String]] = [["-","-","-"],
+                                 ["-","-","-"],
+                                 ["-","-","-"]]
+    
+    func updateBoard() {
+    
+    }
+    
+    func checkForWin() {
+        
+    }
+    
+    
 }
+
+
+
+
+
+
+// Model
+// Matrix that controls state of game (update based on user input) - var
+// 1. Player switch (case?) -> Player one (assigns o), Player 2 (assigns x)
+// mutating func to assign change in matrix,
+// 3. Func for all winning combos (8 Combos), check for win condition. where tag.X == tag.Y == tag.Z.
+
+
+// Controller
+// 1a. Depending on player, change label
+// 1b. Depending on player, change button image when pressed. Disable buttons
+
+
+// View
+// 1a. Sees whose turn based on player (label)
+// 1b. Press button, image based on player
+
