d10f60442bda8b36b1f00ab8e71904dba75d18d5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2019-08-02 16:58:10 -0400

    adds functions to check for win in TicTacToeBrain model

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -2,8 +2,8 @@
 //  TicTacToeBrain.swift
 //  TicTacToe
 //
-//  Created by Alex Paul on 11/8/18.
-//  Copyright © 2018 Pursuit. All rights reserved.
+//  Created by Sunni Tang on 08/1/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
 //
 
 import Foundation
@@ -13,15 +13,52 @@ class TicTacToeBrain {
     var gameBoard: [[String]] = [["-","-","-"],
                                  ["-","-","-"],
                                  ["-","-","-"]]
+    var playerOneCounter = 0
+    var playerTwoCounter = 0
+
     
-    func updateBoard() {
+    func playerOneWins() {
+        playerOneCounter += 1
+    }
     
+    func playerTwoWins() {
+        playerTwoCounter += 1
     }
     
     func checkForWin() {
+        let row1: [String] = [gameBoard[1][1], gameBoard [1][2], gameBoard [1][3]]
+        let row2: [String] = [gameBoard[2][1], gameBoard [2][2], gameBoard [2][3]]
+        let row3: [String] = [gameBoard[3][1], gameBoard [3][2], gameBoard [3][3]]
+        let col1: [String] = [gameBoard[1][1], gameBoard [2][1], gameBoard [3][1]]
+        let col2: [String] = [gameBoard[1][2], gameBoard [2][2], gameBoard [3][2]]
+        let col3: [String] = [gameBoard[1][3], gameBoard [2][3], gameBoard [3][3]]
+        let dia1: [String] = [gameBoard[1][1], gameBoard [2][2], gameBoard [3][3]]
+        let dia2: [String] = [gameBoard[3][1], gameBoard [2][2], gameBoard [1][3]]
+        
+        //horizontal victory
+        if row1 == ["o", "o", "o"] || row2 == ["o", "o", "o"] || row3 == ["o", "o", "o"] {
+            playerOneWins()
+        } else if row1 == ["x", "x", "x"] || row2 == ["x", "x", "x"] || row3 == ["x", "x", "x"] {
+            playerTwoWins()
+        }
+        
+        //vertical
+        if col1 == ["o", "o", "o"] || col2 == ["o", "o", "o"] || col3 == ["o", "o", "o"] {
+            playerOneWins()
+        } else if col1 == ["x", "x", "x"] || col2 == ["x", "x", "x"] || col3 == ["x", "x", "x"]  {
+            playerTwoWins()
+        }
+        
+        //diagonal
+        if dia1 == ["o", "o", "o"] || dia2 == ["o", "o", "o"] {
+            playerOneWins()
+        } else if dia1 == ["x", "x", "x"] || dia2 == ["x", "x", "x"] {
+            playerTwoWins()
+        }
+        
         
     }
-    
+
     
 }
 
