60ed2e169ee05b168a6b548ff642aa1045469b42
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-02 17:48:54 -0400

    adds option for who will start, alternates between turns and replaces buttons with x or o accordiningly

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,20 +9,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
-  override func viewDidLoad() {
+    @IBOutlet weak var button1: GameButton!
+    @IBOutlet weak var button2: GameButton!
+    @IBOutlet weak var button3: GameButton!
+    @IBOutlet weak var button4: GameButton!
+    @IBOutlet weak var button5: GameButton!
+    @IBOutlet weak var button6: GameButton!
+    @IBOutlet weak var button7: GameButton!
+    @IBOutlet weak var button8: GameButton!
+    @IBOutlet weak var button9: GameButton!
+    @IBOutlet weak var whoWillStart: UILabel!
+    @IBOutlet weak var player1: UIButton!
+    @IBOutlet weak var player2: UIButton!
+    @IBOutlet weak var playerTurnStatus: UILabel!
+    var playerTurn1 = ""
+    var playerTurn = WhoGoesFirst()
+    let xIcon:UIImage? = UIImage(named: "x.png")
+    let oIcon:UIImage? = UIImage(named: "o.png")
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
+        whoWillStart.isHidden = false
+        player1.isHidden = false
+        player2.isHidden = false
+        playerTurnStatus.isHidden = true
     // Do any additional setup after loading the view, typically from a nib.
   }
 
-    @IBAction func buttonPressed(_ sender: GameButton) {
+    
+    func gameStuff(symbol: String, button: UIButton) {
+        if symbol == "x" {
+            button.setImage(xIcon, for: UIControl.State.normal)
+            button.isEnabled = false
+            playerTurn1 = "o"
+        } else if symbol == "o" {
+            button.setImage(oIcon, for: UIControl.State.normal)
+            button.isEnabled = false
+            playerTurn1 = "x"
+        }
     }
     
-    @IBAction func player2(_ sender: UIButton) {
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: GameButton) {
+        
+        switch sender.tag {
+        case 1:
+            gameStuff(symbol: playerTurn1, button: button1)
+        case 2:
+            gameStuff(symbol: playerTurn1, button: button2)
+        case 3:
+            gameStuff(symbol: playerTurn1, button: button3)
+        case 4:
+            gameStuff(symbol: playerTurn1, button: button4)
+        case 5:
+            gameStuff(symbol: playerTurn1, button: button5)
+        case 6:
+            gameStuff(symbol: playerTurn1, button: button6)
+        case 7:
+            gameStuff(symbol: playerTurn1, button: button7)
+        case 8:
+            gameStuff(symbol: playerTurn1, button: button8)
+        case 9:
+            gameStuff(symbol: playerTurn1, button: button9)
+        default:
+            print("nothing happened")
+            //check if any win conditions have been met
+        }
+        
     }
-    @IBAction func player1(_ sender: UIButton) {
+    
+
+    @IBAction func whoGoesFirst(_ sender: UIButton) {
+        if sender.tag == 0 {
+            playerTurn1 = "x"
+        } else if sender.tag == 1 {
+            playerTurn1 = "o"
+        }
+        whoWillStart.isHidden = true
+        player1.isHidden = true
+        player2.isHidden = true
     }
+    
     
 }
 
