8bfacc4d9f1dc2c211dd6b2ca68094f283f178ca
Jason Ruan <jasonruan@pursuit.org>
2019-08-04 13:37:13 -0400

    Creates game layout and function to toggle functionality of game buttons

--- TicTacToe/TicTacToe/GameButton.swift
+++ TicTacToe/TicTacToe/GameButton.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class GameButton: UIButton {
-  @IBInspectable var row: Int = 0
-  @IBInspectable var col: Int = 0
+    @IBInspectable var row: Int = 0
+    @IBInspectable var col: Int = 0
+    
+    static func buttonState(_ buttonArray: [GameButton], _ state: Bool) {
+        for button in buttonArray {
+            button.isEnabled = state
+        }
+    }
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    
+    
+    @IBOutlet weak var turnPlayer: UILabel!
+    @IBOutlet var gameButtons: [GameButton]!
+    @IBOutlet weak var resetGame: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        GameButton.buttonState(gameButtons, false)
+        turnPlayer.text = "Welcome! Ready?"
+    }
+    
+    @IBAction func newGame(_ sender: UIButton) {
+        GameButton.buttonState(gameButtons, true)
+    }
+    
 }
 
