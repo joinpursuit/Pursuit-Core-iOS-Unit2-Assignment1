7ef65f6e225f0e90c06df477eb6760377b4e5cee
NormanLiana <liananorman@pursuit.org>
2019-08-10 16:07:09 -0400

    creates gameboard prop with updateBoard method and checkGameState method

--- TicTacToe/TicTacToe/Models/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/Models/TicTacToeBrain.swift
@@ -9,5 +9,50 @@
 import Foundation
 
 class TicTacToeBrain {
-  
+  var gameBoard = [["-", "-", "-"],
+                    ["-", "-", "-"],
+                    ["-", "-", "-"]]
+    
+    func updateGameBoard(player: Players, row: Int, col: Int) {
+        switch player {
+        case .player1:
+            gameBoard[row][col] = "O"
+        case .player2:
+            gameBoard[row][col] = "X"
+        }
+    }
+    
+    func checkGameState() -> GameState {
+        // horizontal check
+        for row in gameBoard {
+            if row == ["O", "O", "O"] {
+                return .player1Wins
+            } else if row == ["X", "X", "X"] {
+                return .player2Wins
+            }
+        }
+        
+        // vertical check
+        for columnIndex in 0..<gameBoard[0].count {
+            var column = [String]()
+            for rowIndex in 0..<gameBoard.count {
+                column.append(gameBoard[rowIndex][columnIndex])
+            }
+        }
+        
+        // diagonal check
+        var diagonal1 = [String]()
+        var diagonal2 = [String]()
+        for i in 0..<gameBoard.count {
+            diagonal1.append(gameBoard[i][i])
+            diagonal2.append(gameBoard[i][gameBoard.count - 1 - i])
+        }
+        if diagonal1 == ["O", "O", "O"] || diagonal2 == ["O", "O", "O"] {
+            return .player1Wins
+        } else if diagonal1 == ["X", "X", "X"] || diagonal2 == ["X", "X", "X"] {
+            return .player2Wins
+        }
+        
+        return .ongoing
+    }
 }
