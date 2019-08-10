b520b037467bb6cfe019ff7043f08f908d1ddaf5
NormanLiana <liananorman@pursuit.org>
2019-08-10 18:43:15 -0400

    adds buttons with contraints, adds IBAction buttonPressed func, adds allButtons Outletand updateButtons func

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,11 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var model = TicTacToeBrain()
+    var player = Players.player1
+    
+    @IBOutlet var allButtons: [GameButton]!
+    
 
-  override func viewDidLoad() {
+    @IBAction func buttonPressed(_ sender: GameButton) {
+        player.alternatePlayers()
+        model.updateGameBoard(player: player, row: sender.row, col: sender.col)
+        let result = model.checkGameState()
+        switch result {
+        case .player1Wins:
+            updateButtons(row: sender.row, col: sender.col, player: player)
+            print("Player 1 Wins")
+        case .player2Wins:
+            updateButtons(row: sender.row, col: sender.col, player: player)
+            print("Player 2 Wins")
+        case .ongoing:
+            updateButtons(row: sender.row, col: sender.col, player: player)
+        case .tie:
+            print("TIE")
+        }
+    }
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
+    func updateButtons(row: Int, col: Int, player: Players) {
+        for button in allButtons {
+            if button.row == row && button.col == col {
+                switch player {
+                case .player1:
+                    button.setTitle("O", for: .normal)
+                    button.isEnabled = false
+                case .player2:
+                    button.setTitle("X", for: .normal)
+                    button.isEnabled = false
+                }
+            }
+        }
+    }
 
 
 }
