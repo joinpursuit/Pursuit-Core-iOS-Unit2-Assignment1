a64037a41b33f52d589ccc2cd3fd949680dfaab8
Ian <ian@Ians-MacBook-Pro.local>
2019-08-12 00:35:40 -0400

    it might not be pretty but it works

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -11,3 +11,40 @@ import Foundation
 class TicTacToeBrain {
   
 }
+
+
+
+//  NOTES ********************************
+
+//THIS ENUM DECIDES WHAT STATE THE GAME IS IN
+// THIS IS USED IN THE FUNCTION IN THE NEXT SECTION TO SWITCH CASE AND DETERMINE AFTER EACH MOVE IF A USER HAS WON OR IF THE GAME WILL CONTINUE.
+//
+//enum GameState {
+//  case player1wins
+//  case player2wins
+//  case ongoing
+//  case tie
+//}
+//
+//
+//
+//********************************
+//
+
+//@IBAction func buttonPressed(_ sender: GameButton) {
+//model.updateGameBoard(player: player, row: sender.row, col: sender.col)
+//let result = model.checkGameState()
+//switch result {
+//case .player1wins:
+//  updateButtons(row: sender.row, col: sender.col, player: player)
+//  print("Player 1 wins!!!")
+//case .player2wins :
+//  updateButtons(row: sender.row, col: sender.col, player: player)
+//  print("Player 2 wins!!!")
+//case .ongoing:
+//  updateButtons(row: sender.row, col: sender.col, player: player)
+//  player.alternate()
+//case .tie:
+//  print("TIE")
+//}
+//}
