4a8b701073e9dcc8ffbf288599283b839e83f5f2
Eddie O'Neill <ejosephoneill@gmail.com>
2019-10-31 16:53:58 -0400

    completed TicTacToe project

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,47 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class TicTacToeBrain {
-  
+    var winingCombinations = [[1, 2, 3],
+                              [4, 5, 6],
+                              [7, 8, 9],
+                              [1, 4, 7],
+                              [2, 5, 8],
+                              [3, 6, 9],
+                              [1, 5, 9],
+                              [3, 5, 7]]
+    var buttonX: Set<Int> = []
+    var buttonO: Set<Int> = []
+    
+    func winningPatern(_ xoButtons: [UIButton], _ gameStatus: UILabel) {
+        for winingCombination in winingCombinations {
+            if buttonX.isSuperset(of: winingCombination) {
+                gameStatus.text = "Player1 Wins!"
+                for button in xoButtons {
+                    button.isEnabled = false
+                }
+                break
+            } else if buttonO.isSuperset(of: winingCombination) {
+                gameStatus.text = "Player2 Wins!"
+                for button in xoButtons {
+                    button.isEnabled = false
+                }
+                break
+            } else if buttonX.count >= 5 {
+                gameStatus.text = "Draw"
+                break
+            }
+        }
+    }
+    
+    func resetGame(_ xoButtons: [UIButton]) {
+        for button in xoButtons {
+            button.setBackgroundImage(nil, for: .normal)
+            button.isEnabled = true
+        }
+        buttonX.removeAll()
+        buttonO.removeAll()
+    }
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var xoButtons: [UIButton]!
+    @IBOutlet weak var gameStatus: UILabel!
+    
+    var gameBrain = TicTacToeBrain()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        gameStatus.text = "It's Player1's Turn"
+        for button in xoButtons {
+            button.setTitleColor(.clear, for: .normal)
+        }
+    }
+    
+    @IBAction func xoAction(_ sender: UIButton) {
+        if gameBrain.buttonX.count == gameBrain.buttonO.count {
+            sender.setBackgroundImage(UIImage(named: "ticTacToeX"), for: .normal)
+            gameBrain.buttonX.insert(sender.tag)
+            sender.isEnabled = false
+            gameStatus.text = "It's Player2's Turn"
+        } else {
+            sender.setBackgroundImage(UIImage(named: "ticTacToeO"), for: .normal)
+            gameBrain.buttonO.insert(sender.tag)
+            sender.isEnabled = false
+            gameStatus.text = "It's Player1's Turn"
+        }
+        gameBrain.winningPatern(xoButtons, gameStatus)
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        gameBrain.resetGame(xoButtons)
+        gameStatus.text = "It's Player1's Turn"
+    }
 }
 
