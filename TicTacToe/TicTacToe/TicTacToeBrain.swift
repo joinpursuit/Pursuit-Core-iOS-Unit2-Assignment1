a0d90a65d9c56fc38000f787ea99f1d6ba71b18c
Bienbenido Angeles <bienbenido.angeles@gmail.com>
2019-11-07 08:04:34 -0500

    Added Game Logic

--- TicTacToe/TicTacToe/TicTacToeBrain.swift
+++ TicTacToe/TicTacToe/TicTacToeBrain.swift
@@ -6,8 +6,87 @@
 //  Copyright © 2018 Pursuit. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class TicTacToeBrain {
-  
+    
+    //instance vars
+    var player = "X"
+    var board = [[String]](repeating: [String](repeating: String(), count: 3), count: 3)
+    var previousPick = String()
+    var rWin = 0, cWin:Int = 0
+    var typeWin = String()
+    
+    //methods
+    func switchPlayer(){
+        if player == "X" {
+            player = "O"
+        } else  {
+            player = "X"
+        }
+    }
+    
+    //Checks for winning conditions.
+    func checkForWin(player: String) -> Bool{
+        for (index, rowValues) in board.enumerated(){
+            //Checking for 3 horizontal (row) markers.
+            if(board[index][0] == player && board[index][1] == player && board[index][2] == player) {
+                rWin = index
+                typeWin = "row"
+                return true
+            }
+            
+            //Checking for 3 vertical (column) markers.
+            if(board[0][index] == player && board[1][index] == player && board[2][index] == player) {
+                cWin = index;
+                typeWin = "col";
+                return true
+            }
+            
+            //Checking for 2 diagonal markers.
+            if(board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+                typeWin = "d1"
+                return true
+            }
+            if(board[2][0] == player && board[1][1] == player && board[0][2] == player) {
+                typeWin = "d2"
+                return true
+            }
+        }
+        return false
+    }
+    
+    //Cycles through an array meant to present game board.
+    //Board is not filled if there are any null instances.
+    func checkBoardFilled() -> Bool{
+        if board.isEmpty{
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    //Text shown for a tie, when the board is filled up.
+    func boardFilled(uiLabel: UILabel) {
+        //No need to disable buttons since they will be all pressed
+        //and disabled already.
+        uiLabel.text = "Game over! No more available cells on game board.\n It's a tie!"
+    }
+    
+    //Once a win is detected, game board is disabled.
+    func winDetected(player: String, arrButtons: [UIButton], uiLabel: UILabel){
+        for button in arrButtons{
+            button.isEnabled = false
+        }
+        uiLabel.text = "Player \(player) won! Game over!"
+    }
+    
+    //Method to print game board to console after each move.
+    //Was used to test if 'X' and 'O' were registering correctly in array.
+    func printBoard() {
+        for (index, row) in board.enumerated(){
+            print(row)
+        }
+    }
+    
 }
--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  override func viewDidLoad() {
+    @IBOutlet var gameButtons: [GameButton]!
+    @IBOutlet weak var userFeedBack: UILabel!
+    
+    override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
-  }
+    }
 
-
+    @IBAction func gameButtonPressed(_ gameButton: GameButton) {
+        gameButton.isEnabled = false
+        
+    }
+    
+    @IBAction func resetGame(_ sender: UIButton) {
+        
+    }
+    
 }
 
