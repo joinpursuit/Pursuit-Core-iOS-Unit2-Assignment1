fdf016d71b3d571544b6f54b38eed20478c823ab
Yuliia Engman <yuliia.engman@gmail.com>
2019-11-07 21:02:22 -0500

    completed game

--- TicTacToe/TicTacToe/ViewController.swift
+++ TicTacToe/TicTacToe/ViewController.swift
@@ -13,52 +13,48 @@ class ViewController: UIViewController {
     
     @IBOutlet var gameButtonsOutletCollection: [UIButton]!
     
-    
     @IBOutlet weak var gameStatusLabel: UILabel!
     
     var activePlayer = 1 // Cross
     var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     
     let winningCombinations = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
-    var gameIsActive = true
     var totalScorePlayer1 = 0
     var totalScorePlayer2 = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       gameStatusLabel.text = """
-       Welcome to Tic Tac Toe game!
-       Player 1 starts with X.
-       """
-      }
+        gameStatusLabel.text = """
+        Welcome to Tic Tac Toe game!
+        Player 1 starts with X.
+        """
+    }
     
-    
-
     @IBAction func gameButtonPressed(_ gameButton: GameButton) {
-        if gameState[gameButton.tag - 1] == 0 && gameIsActive == true { //
+        if gameState[gameButton.tag - 1] == 0 {
             
             gameState[gameButton.tag - 1] = activePlayer
             
             if activePlayer == 1 {
                 
-                    gameButton.setImage(UIImage(named: "Cross.png"), for: UIControl.State.normal)
-                    activePlayer = 2
-gameStatusLabel.text = """
+                gameButton.setImage(UIImage(named: "Cross.png"), for: UIControl.State.normal)
+                activePlayer = 2
+                gameStatusLabel.text = """
                 Player two's turn.
                 Place an O.
                 """
-                } else {
-                    gameButton.setImage(UIImage(named: "Circle.png"), for: UIControl.State.normal)
-                    activePlayer = 1
+            } else {
+                gameButton.setImage(UIImage(named: "Circle.png"), for: UIControl.State.normal)
+                activePlayer = 1
                 gameStatusLabel.text = """
                 Player one's turn.
                 Place an X.
                 """
-                       }
+            }
         }
         for combination in winningCombinations {
             if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] {
-                gameIsActive = false
+               
                 if gameState[combination[0]] == 1 {
                     totalScorePlayer1 += 1
                     gameStatusLabel.text = """
@@ -68,51 +64,35 @@ gameStatusLabel.text = """
                     for gameButtonOutlet in gameButtonsOutletCollection {
                         gameButtonOutlet.isEnabled = false
                     }
-                    gameIsActive = false
                     
-                } else {
+                } else if gameState[combination[0]] == 2 {
                     totalScorePlayer2 += 1
                     gameStatusLabel.text = """
                     CIRCLE WON!
                     YOUR TOTAL SCORE IS \(totalScorePlayer2)!
                     """
-                   for gameButtonOutlet in gameButtonsOutletCollection {
+                    for gameButtonOutlet in gameButtonsOutletCollection {
                         gameButtonOutlet.isEnabled = false
                     }
-                    gameIsActive = false
                     
+                } else {
+                    gameStatusLabel.text = """
+                    THE GAME IS TIGHT.
+                    START PLAYING A NEW GAME.
+                    """
+                    playAgainButton.isHidden = false
+                    gameStatusLabel.isHidden = false
                 }
                 
                 playAgainButton.isHidden = false
                 gameStatusLabel.isHidden = false
             }
         }
-        gameIsActive = false
-
-        for i in gameState {
-            if i == 0 {
-                gameIsActive = true
-                break
-            }
-        }
-        if gameIsActive == false {
-            gameStatusLabel.text = """
-THE GAME IS TIGHT.
-START PLAYING A NEW GAME.
-"""
-            playAgainButton.isHidden = false
-            gameStatusLabel.isHidden = false
-//           for gameButtonOutlet in gameButtonsOutletCollection {
-//                gameButtonOutlet.isEnabled = true
-//            }
-            
-        }
     }
     
     @IBOutlet weak var playAgainButton: UIButton!
     @IBAction func playAgain(_ sender: UIButton) {
         gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        gameIsActive = true
         activePlayer = 1 // Cross
         
         playAgainButton.isHidden = false
@@ -123,11 +103,8 @@ START PLAYING A NEW GAME.
             button.setImage(nil, for:   UIControl.State.normal)
             
             for gameButtonOutlet in gameButtonsOutletCollection {
-                         gameButtonOutlet.isEnabled = true
+                gameButtonOutlet.isEnabled = true
             }
         }
     }
-    
-
-
 }
